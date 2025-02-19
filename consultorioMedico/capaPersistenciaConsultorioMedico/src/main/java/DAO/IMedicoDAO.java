a521e18d4a4c1f885d0c1695a5b77fa248a54cbd
Juamp-i5 <juan.olivarria252886@potros.itson.edu.mx>
2025-02-19 16:47:31 -0700

    DireccionPacienteDAO y MedicoDAO

---
+++ consultorioMedico/capaPersistenciaConsultorioMedico/src/main/java/DAO/IMedicoDAO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAO;
+
+import entidades.Medico;
+import excepciones.PersistenciaException;
+import java.util.List;
+
+/**
+ *
+ * @author Jp
+ */
+public interface IMedicoDAO {
+    
+    boolean agregarMedico(Medico medico) throws PersistenciaException;
+
+    Medico consultarMedico(int idMedico) throws PersistenciaException;
+
+    List<Medico> consultarTodosLosMedicos() throws PersistenciaException;
+
+    boolean actualizarMedico(Medico medico) throws PersistenciaException;
+
+    boolean existeMedico(int idMedico) throws PersistenciaException;
+}
---
+++ consultorioMedico/capaPersistenciaConsultorioMedico/src/main/java/DAO/MedicoDAO.java
@@ -0,0 +1,121 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import conexion.Conexion;
+import entidades.Medico;
+import excepciones.PersistenciaException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Jp
+ */
+public class MedicoDAO implements IMedicoDAO {
+
+    @Override
+    public boolean agregarMedico(Medico medico) throws PersistenciaException {
+        String query = "INSERT INTO consultas_medicas.medico (especialidad, cedula_profesional, estado) VALUES (?, ?, ?);";
+
+        try (Connection conexion = Conexion.getConnection(); PreparedStatement ps = conexion.prepareStatement(query)) {
+            ps.setString(1, medico.getEspecialidad());
+            ps.setString(2, medico.getCedulaProfesional());
+            ps.setString(3, medico.getEstado());
+
+            int affectedRows = ps.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            throw new PersistenciaException("Error al agregarMedico: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Medico consultarMedico(int idMedico) throws PersistenciaException {
+        String query = "SELECT * FROM consultas_medicas.medico WHERE id_medico = ?;";
+        Medico medico = null;
+
+        try (Connection conexion = Conexion.getConnection(); PreparedStatement ps = conexion.prepareStatement(query)) {
+            ps.setInt(1, idMedico);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    medico = new Medico(
+                            rs.getInt("id_medico"),
+                            rs.getString("especialidad"),
+                            rs.getString("cedula_profesional"),
+                            rs.getString("estado")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            throw new PersistenciaException("Error al consultarMedico: " + e.getMessage());
+        }
+
+        return medico;
+    }
+
+    @Override
+    public List<Medico> consultarTodosLosMedicos() throws PersistenciaException {
+        String query = "SELECT * FROM consultas_medicas.medico;";
+        List<Medico> medicos = new ArrayList<>();
+
+        try (Connection conexion = Conexion.getConnection(); PreparedStatement ps = conexion.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Medico medico = new Medico(
+                        rs.getInt("id_medico"),
+                        rs.getString("especialidad"),
+                        rs.getString("cedula_profesional"),
+                        rs.getString("estado")
+                );
+                medicos.add(medico);
+            }
+        } catch (SQLException e) {
+            throw new PersistenciaException("Error al consultarTodosLosMedicos: " + e.getMessage());
+        }
+
+        return medicos;
+    }
+
+    @Override
+    public boolean actualizarMedico(Medico medico) throws PersistenciaException {
+        String query = "UPDATE consultas_medicas.medico SET especialidad = ?, cedula_profesional = ?, estado = ? WHERE id_medico = ?;";
+
+        try (Connection conexion = Conexion.getConnection(); PreparedStatement ps = conexion.prepareStatement(query)) {
+            ps.setString(1, medico.getEspecialidad());
+            ps.setString(2, medico.getCedulaProfesional());
+            ps.setString(3, medico.getEstado());
+            ps.setInt(4, medico.getIdMedico());
+
+            int affectedRows = ps.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            throw new PersistenciaException("Error al actualizarMedico: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean existeMedico(int idMedico) throws PersistenciaException {
+        String query = "SELECT COUNT(*) AS total FROM consultas_medicas.medico WHERE id_medico = ?;";
+
+        try (Connection conexion = Conexion.getConnection(); PreparedStatement ps = conexion.prepareStatement(query)) {
+            ps.setInt(1, idMedico);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("total") > 0;
+                }
+            }
+        } catch (SQLException e) {
+            throw new PersistenciaException("Error en existeMedico: " + e.getMessage());
+        }
+
+        return false;
+    }
+}
