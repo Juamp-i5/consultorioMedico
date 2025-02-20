c28a699453ae48a52a0f8419a90363f576fbf477
lucianobarceloo <luciano.barcelo252086@potros.itson.edu.mx>
2025-02-19 21:12:10 -0700

    insertarMedicoBO

---
+++ consultorioMedico/capaNegocioConsultorioMedico/src/main/java/DTO/MedicoNuevoDTO.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Admin
+ */
+public class MedicoNuevoDTO {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String especialidad;
+    private String cedulaProfesional;
+    private String estado;
+    private String contrasenia;
+
+    public MedicoNuevoDTO() {
+    }
+
+    public MedicoNuevoDTO(String nombre, String apellidoPaterno, String apellidoMaterno, String especialidad, String cedulaProfesional, String estado, String contrasenia) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.especialidad = especialidad;
+        this.cedulaProfesional = cedulaProfesional;
+        this.estado = estado;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getCedulaProfesional() {
+        return cedulaProfesional;
+    }
+
+    public void setCedulaProfesional(String cedulaProfesional) {
+        this.cedulaProfesional = cedulaProfesional;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    
+
+  
+    
+    
+}
