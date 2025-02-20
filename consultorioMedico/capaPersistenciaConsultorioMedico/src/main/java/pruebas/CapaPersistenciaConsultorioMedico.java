ce129d832c27e59d6b315b98a5ced22213d9761a
lucianobarceloo <luciano.barcelo252086@potros.itson.edu.mx>
2025-02-19 17:26:36 -0700

    Prueba BUENA

--- consultorioMedico/capaPersistenciaConsultorioMedico/src/main/java/pruebas/CapaPersistenciaConsultorioMedico.java
+++ consultorioMedico/capaPersistenciaConsultorioMedico/src/main/java/pruebas/CapaPersistenciaConsultorioMedico.java
@@ -11,6 +11,6 @@ package pruebas;
 public class CapaPersistenciaConsultorioMedico {
 
     public static void main(String[] args) {
-        System.out.println("asfddassdhfgkjsadg,kashgedgkjqwehglkqwejehg,kqwejehg");
+        System.out.println("Ahora si ya la buena");
     }
 }
