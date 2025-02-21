0f45a63a8d9f1044e4585b62f64309c6d58addc6
janotheboss75 <82925930+janotheboss75@users.noreply.github.com>
2025-02-21 08:40:12 -0700

    Create ICita.java
    La agregue manualmente por que dice que genera conflicto

---
+++ consultorioMedico/capaPersistenciaConsultorioMedico/src/main/java/DAO/ICita.java
@@ -0,0 +1,43 @@
+package DAO;
+
+import entidades.Cita;
+import entidades.Medico;
+import entidades.Paciente;
+import excepciones.PersistenciaException;
+import java.util.List;
+
+/**
+ *
+ * @author janot
+ */
+public interface ICita {
+    /**
+     * Agenda una cita y la guarda en la base de datos
+     * 
+     * @param cita El cual tendra toda la info a guardar en la bd
+     * @return true si se agenda la cita y se guarda en la bd, y false en caso
+     * contrario
+     * @throws PersistenciaException Si no se logra hacer la cita
+     */
+    public boolean agendarCita(Cita cita) throws PersistenciaException;
+    
+    /**
+     * Cancela una cita de la base de datos, cambiando su estado a cancelado
+     * 
+     * @param idCita de la cita que se desea cancelar
+     * @return True si logra hacer la cancelacion y false en caso contrario
+     * @throws PersistenciaException 
+     */
+    public boolean cancelarCita(int idCita) throws PersistenciaException;
+    
+
+    /**
+     * Obtiene todas las citas que estan activas del paciente que tiene ese id
+     * 
+     * @param id Id del paciente del que se quiere saber sus citas disponibles
+     * @return Una lista con todas las citas activas del paciente
+     * @throws PersistenciaException Si no se logra obtener el listado
+     */
+    public List<Cita> obtenerCitasActivasPaciente(int idPaciente) throws PersistenciaException;
+    
+}
